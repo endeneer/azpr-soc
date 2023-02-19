181a8483c1aba8ea40ea227a78996e6c3c8035e0
Tan En De <ende.tan@starfivetech.com>
2023-02-19 18:02:25 +0800

    hdl: lib: Add Quartus-generated IP for Cyclone II (dcm, dpram, sprom)
    Side note:
    Since the initial rtl code is from zhangly/azpr_cpu GitHub repo,
    and the repo has already modified rtl to use Quartus-generated IP,
    this commit only has to generate IP for Cyclone II.
    You may refer to clk_gen.v, spm.v, and rom.v to see the changes to use Quartus-generated IP.

---
+++ 1-hdl/lib/dcm/altera_dcm_inst.v
@@ -0,0 +1,7 @@
+altera_dcm	altera_dcm_inst (
+	.areset ( areset_sig ),
+	.inclk0 ( inclk0_sig ),
+	.c0 ( c0_sig ),
+	.c1 ( c1_sig ),
+	.locked ( locked_sig )
+	);
---
+++ 1-hdl/lib/sprom/altera_sprom_inst.v
@@ -0,0 +1,5 @@
+altera_sprom	altera_sprom_inst (
+	.address ( address_sig ),
+	.clock ( clock_sig ),
+	.q ( q_sig )
+	);
